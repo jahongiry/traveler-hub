acc3f5116be8edf5cab3beb3bec033e378b0aba9
safar ali <miqra019@gmail.com>
2022-07-18 18:23:53 +0500

    Create a basic UI for the missions

--- src/components/Missons.js
+++ src/components/Missons.js
@@ -1,5 +1,64 @@
-const Missions = () => {
-  return "Test Mission";
-};
+const Missions = () => (
+  <div className="missions-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th className="discription">Description</th>
+          <th>Status</th>
+          <th>Join</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="mission-name">loram ipsom</td>
+          <td>
+            Thaicom is the name of a series of communications satellites
+            operated from Thailand, and also the name of Thaicom Public
+            Company Limited, which is the company that owns and operates
+            the Thaicom satellite fleet and other telecommunication businesses
+            in Thailand and throughout the Asia-Pacific region. The satellite
+            projects were named Thaicom by the King of Thailand, His Majesty
+            the King Bhumibol Adulyadej, as a symbol of the linkage between
+            Thailand and modern communications technology.
+          </td>
+          <td><button type="submit">Not A Member</button></td>
+          <td><button type="submit">Join Mission</button></td>
+        </tr>
+        <tr>
+          <td>loram ipsom</td>
+          <td>
+            Thaicom is the name of a series of communications
+            satellites operated from Thailand, and also the name of
+            Thaicom Public Company Limited, which is the company
+            that owns and operates the Thaicom satellite fleet and
+            other telecommunication businesses in Thailand and th
+            roughout the Asia-Pacific region. The satellite projects
+            were named Thaicom by the King of Thailand, His Majesty
+            the King Bhumibol Adulyadej, as a symbol of the linkage
+            between Thailand and modern communications technology.
+          </td>
+          <td><button type="submit">Not A Member</button></td>
+          <td><button type="submit">Join Mission</button></td>
+        </tr>
+        <tr>
+          <td>loram ipsom</td>
+          <td>
+            Thaicom is the name of a series of communications
+            satellites operated from Thailand, and also the name of
+            Thaicom Public Company Limited, which is the company that
+            owns and operates the Thaicom satellite fleet and other
+            telecommunication businesses in Thailand and throughout
+            the Asia-Pacific region. The satellite projects were named
+            Thaicom by the King of Thailand, His Majesty the King Bhumibol
+            Adulyadej, as a symbol of the linkage and modern communications technology.
+          </td>
+          <td><button type="submit">Not A Member</button></td>
+          <td><button type="submit">Join Mission</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+);
 
 export default Missions;
