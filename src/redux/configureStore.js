fead0178c55614c60be32f73fbc8e4bc46e079e4
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2022-07-19 16:00:28 +0500

    Add fetch data feature

---
+++ src/redux/configureStore.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { rocketInfo } from "./store";
+
+const store = configureStore({
+  reducer: { rocket: rocketInfo.reducer },
+});
+export default store;
